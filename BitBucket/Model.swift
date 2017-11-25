fcb13eb3f5e3583a0fa25b182564462c75361fa5
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-11-25 20:56:21 +0800

    Data Model Added

---
+++ BitBucket/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  BitBucket
+//
+//  Created by Francis Jemuel Bergonia on 25/11/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import Foundation
